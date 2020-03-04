38e690d0d08e2af1c503f5c28768a8a317d2ade3
llARXANGELll <aramh@inbox.ru>
2020-03-05 00:28:45 +0300

    В процессе

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,5 +1,9 @@
+import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -13,18 +17,22 @@ public class YandexTest {
 //        System.getProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 //    }
 
+    WebDriver webDriver = new ChromeDriver();
     @Test
     public void yandexx() {
         System.getProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://yandex.ru/ncr");
-        webDriver.quit();
+        webDriver.get("https://yandex.ru");
+        WebElement input = webDriver.findElement(By.name("text"));
+        input.sendKeys("руддщ цкщдв",Keys.ENTER);
+
+
 
     }
 
-//    public void driverOut(){
-//        webDriver.quit();
-//    }
+    @After
+    public void driverOut(){
+        webDriver.quit();
+    }
 
 
 }
