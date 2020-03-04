955795b74e89a368cd405c24a641c093fb694ad3
llARXANGELll <aramh@inbox.ru>
2020-03-04 23:54:24 +0300

    Вернул JUnit

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,16 +1,30 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+
 
 
 
 public class YandexTest {
+
+//    public WebDriver webDriver = new ChromeDriver();
+//    @BeforeTest
+//    public void driverLaunch(){
+//        System.getProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+//    }
 
     @Test
     public void yandexx() {
         System.getProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://yandex.ru/ncr");
+        webDriver.quit();
+
     }
+
+//    public void driverOut(){
+//        webDriver.quit();
+//    }
+
 
 }
