fa160a566c37983e37bfa49bc0b4d19a4573a572
llARXANGELll <aramh@inbox.ru>
2020-03-04 23:30:58 +0300

    EcnУстановил и запустил драйвер

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,2 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
 public class YandexTest {
+
+    @Test
+    public void yandexx() {
+        System.getProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://yandex.ru/ncr");
+    }
+
 }
